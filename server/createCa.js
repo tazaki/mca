a09edd44aa20160ec17a5d182200c88416cad5b7
Justin McMillion <justin.mcmillion@aquilent.com>
2015-03-18 11:38:11 -0400

    not sure how that last commit worked...templates should work now

--- server/createCa.js
+++ server/createCa.js
@@ -1,10 +1,11 @@
 Meteor.methods({
   createCa: function (cn, email, locale_name, org_name, state, country) {
 
-    writeTemplate();
+    tempData = { "name": "Alan", "hometown": "Somewhere, TX","kids": [{"name": "Jimmy", "age": "12"}, {"name": "Sally", "age": "4"}]};
+    writeTemplate('templates/openssl.conf.hb', tempData);
 
     return 'ran';
-    
+
     // Create directory/file structure
     /*
     console.log('Creating files and directories for ca.');
