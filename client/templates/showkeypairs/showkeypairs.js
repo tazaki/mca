ab96ca919a2f0b27b1a573fed532897260827d25
Justin McMillion <justin.mcmillion@aquilent.com>
2015-03-25 15:26:09 -0400

    finished issue 4

--- client/templates/showkeypairs/showkeypairs.js
+++ client/templates/showkeypairs/showkeypairs.js
@@ -9,3 +9,12 @@ Template.showkeypairs.helpers({
     return Keypairs.find({});
   }
 });
+
+Template.showkeypairs.events({
+  'click .button_download_kpcert': function (event) {
+    window.location.href = 'keypaircert/' + this._id;
+  },
+  'click .button_download_kpkey': function (event) {
+    window.location.href = 'keypairkey/' + this._id;
+  },
+});
