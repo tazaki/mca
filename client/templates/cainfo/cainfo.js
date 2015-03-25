5c01b4ddfe986b7cf001bdf7cab4c7b72e4ec9a5
Justin McMillion <justinmcmil@gmail.com>
2015-03-24 20:52:55 -0400

    removed collectionfs. I dont need it!

--- client/templates/cainfo/cainfo.js
+++ client/templates/cainfo/cainfo.js
@@ -1,5 +1,8 @@
 Template.cainfo.events({
   "click #button_createca": function (event) {
     Router.go('createca');
+  },
+  'click #button_download_ca': function (event) {
+    window.location.href = 'cacert/' + this._id;
   }
 });
--- server/addCa.js
+++ server/addCa.js
@@ -1,26 +1,16 @@
 Meteor.methods({
   addCa: function (cn, email, locale_name, org_name, state, country, expires) {
 
-    file_location = mca_root_path + '/cacrt.pem';
-    fs = Npm.require('fs');
-    file = fs.readFileSync(file_location);
-    fsFile = new FS.File(file_location);
-    fsFile.metadata = {
+    Cas.insert({
       cn: cn,
       email: email,
       localename: locale_name,
       orgname: org_name,
       state: state,
       country: country,
-      expires: expires
-    };
-
-    Cas.insert(fsFile,function(err,result){
-      if(!err){
-        console.log('Saved file to: ' + result.collectionName);
-        //console.log(result) //you should get an id here since the full object take more less 10 sec to upload
-      }
+      expires: expires,
+      cert: mca_root_path + 'cacrt.pem',
+      key: mca_root_path + 'cakey.pem'
     });
-
   },
 });
