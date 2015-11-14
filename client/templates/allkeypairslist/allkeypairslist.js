805e77e6f5f4a2bf34818420cf66d07396ea5c87
Justin McMillion <justin.mcmillion@sunayu.com>
2015-11-13 19:22:44 -0500

    updates

--- client/templates/allkeypairslist/allkeypairslist.js
+++ client/templates/allkeypairslist/allkeypairslist.js
@@ -13,14 +13,33 @@ Template.allkeypairslist.helpers({
       fields: [
         { key: 'cn', label: 'Common Name' },
         { key: 'created', label: 'Created', tmpl: Template.kpcreatedtemp },
+        { key: null, label: 'Created By', tmpl: Template.kpcreatedby },
+        { key: null, label: 'CA', tmpl: Template.kpca },
         { key: 'expires', label: 'Expires', tmpl: Template.kpexpiredtemp },
-        { key: null, label: 'Downloads', tmpl: Template.kpbuttons}
+        { key: null, label: 'Downloads', tmpl: Template.kpdownloads },
+        { key: null, label: 'Actions', tmpl: Template.kpactions }
       ]
     };
   }
 });
 
 Template.allkeypairslist.events({
+  'click .button_download_kpcert': function (event) {
+    return false;
+    //window.location.href = 'keypaircert/' + this._id;
+  },
+  'click .button_download_kpkey': function (event) {
+    return false;
+    //window.location.href = 'keypairkey/' + this._id;
+  },
+  'click .button_kp_delete': function (event) {
+    return false;
+    //window.location.href = 'keypairkey/' + this._id;
+  },
+  'click .button_kp_renew': function (event) {
+    return false;
+    //window.location.href = 'keypaircert/' + this._id;
+  },
   "click tbody tr": function (event) {
     Router.go('keypairs.show', {_id: this._id});
   }
--- client/templates/keypairlist/keypairlist.js
+++ client/templates/keypairlist/keypairlist.js
@@ -10,13 +10,30 @@ Template.keypairlist.helpers({
         { key: 'cn', label: 'Common Name' },
         { key: 'created', label: 'Created', tmpl: Template.kpcreatedtemp },
         { key: 'expires', label: 'Expires', tmpl: Template.kpexpiredtemp },
-        { key: null, label: 'Downloads', tmpl: Template.kpbuttons}
+        { key: null, label: 'Downloads', tmpl: Template.kpdownloads },
+        { key: null, label: 'Actions', tmpl: Template.kpactions }
       ]
     };
   }
 });
 
 Template.keypairlist.events({
+  'click .button_download_kpcert': function (event) {
+    return false;
+    //window.location.href = 'keypaircert/' + this._id;
+  },
+  'click .button_download_kpkey': function (event) {
+    return false;
+    //window.location.href = 'keypairkey/' + this._id;
+  },
+  'click .button_kp_delete': function (event) {
+    return false;
+    //window.location.href = 'keypairkey/' + this._id;
+  },
+  'click .button_kp_renew': function (event) {
+    return false;
+    //window.location.href = 'keypaircert/' + this._id;
+  },
   "click tbody tr": function (event) {
     Router.go('keypairs.show', {_id: this._id});
   }
